19af29a7936df68ed9109615fbc139d63320f4aa
MolakaRoopa <roopareddy8978@gmail.com>
2023-11-06 23:11:46 +0530

    cards

--- src/App.js
+++ src/App.js
@@ -1,37 +1,32 @@
 import React,{useState} from "react";
-import axios from "axios";
-import { useEffect } from "react";
+import "./App.css"
+import {Header}from "./Header"
+import{Food}from"./food"
+import { FoodCard } from "./foodcard";
 
-const App=()=>{
-    const [data,setData] =useState([]);
 
-
-    useEffect(()=>{
-        axios.get ("https://jsonplaceholder.typicode.com/users")
-        .then((res)=>{
-            setData(res.data)
-        
-        })
-        },[])
+const App= ()=>{
+    const [foodList,updateFoodList]=useState([{foodUrl:"https://static.toiimg.com/thumb/61048461.cms?imgsize=1981854&width=800&height=800" ,
+foodName:"PaniPuri",id:"1"},
+{foodUrl:"https://images.indianexpress.com/2022/09/biryani_getty.jpg?w=414",
+foodName:"Biryani",id:"2"},
+{foodUrl:"https://images.indianexpress.com/2020/04/jalebis-759.jpg",
+foodName:"Jalebi",id:"3"},
+{foodUrl:"https://images.indianexpress.com/2020/04/kaju-katli-759.jpg",
+foodName:"KajuKatli",id:"4"},
+{foodUrl:"https://2.bp.blogspot.com/_8ozzohLEaK4/SED2aX92joI/AAAAAAAAA5U/Hby_J9wZF60/w1200-h630-p-k-no-nu/P1010008-8.JPG",
+foodName:"Jeedipappu Kodikura",id:"5"}
+])
+return(
+    <>
     
-
-    return(
-        <>
-        <ol>
-        {data.map((user,i)=>{
-            return<li key={i}>{user.name}</li>
-        })}
-        </ol>
-        </>
-    )}
-            
-
-      
-   
-
-
-
-
- 
-
+    <Header/>
+    <Food/>
+    { foodList.map( (item,i)=>{
+        console.log(item)
+return<FoodCard Name={item.foodName}Img={item.foodUrl} Link={item.id} />
+    })}
+    </>
+)
+}
 export default App;
---
+++ src/food.js
@@ -0,0 +1,20 @@
+export const Food=()=>{
+return(
+    <>
+    <div className="container-fluid ">
+  <div className="row ">
+  <div className="col-12 pt-4 ">
+    <h1>
+        Top Five Favourite Food Items
+    </h1></div>
+    <div className="col ">
+    <ul>
+      <a className="text-dark text-decoration-none" href="./index.html#1"><li>PaniPuri</li></a>
+      <a className="text-dark text-decoration-none"href="./index.html#2"><li>Biryani</li></a>
+      <a className="text-dark text-decoration-none"href="./index.html#3"><li>Jalebi</li></a>
+      <a className="text-dark text-decoration-none"href="./index.html#4"><li>KajuKatli</li></a>
+      <a className="text-dark text-decoration-none"href="./index.html#5"><li>Jeedipappu Kodikura</li></a>
+    </ul></div></div></div>
+    </>
+)
+}
