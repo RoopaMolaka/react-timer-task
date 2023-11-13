e379aa518492ae3fe7c20ec27fb89c6cd26b6df0
MolakaRoopa <roopareddy8978@gmail.com>
2023-11-13 21:31:55 +0530

    changes made

--- src/App.js
+++ src/App.js
@@ -1,36 +1,32 @@
-// 
-import React, { useState, useEffect } from 'react';
-import "./App.css"
-
-const App = () => {
-  const [count, setCount] = useState(0);
-  const [state, setState] = useState(false);
-
-  useEffect(() => {
-     if (state) {
-        let interval = setInterval(() => {
-        setCount((prevCount) => prevCount + 1);
-      }, 500);
-    return () => {
-      clearInterval(interval);
-    }};
-  }, [state]); 
-
-  const startButton = () => {
+ import React from "react";
+ import { useState,useEffect } from "react";
+ const App =()=>{
+    const [count,setCount]=useState(0)
+    const [state,setState]=useState(false)
+ useEffect(()=>{
+    let counter=""
+    if(state){
+        counter =setInterval(()=>{ 
+            setCount((prevCount)=>prevCount+1)},500)
+            
+    }else{
+        clearInterval(counter)
+    } return()=>clearInterval(counter)
+  },[state])
+  const startButton=()=>{
     setState(true)
   }
-
-  const stopButton = () => {
+  const stopButton=()=>{
     setState(false)
-  };
+  }
 
-  return (
-    <div>
-      <p  >Count: {count}</p>
-      <button  onClick={startButton}>Start</button>
-      <button   onClick={stopButton}>Stop</button>
-    </div>
-  )
-}
-
-export default App;
+    return(
+        <>
+        <p>Count:{count}</p>
+        <button onClick={()=>{startButton()}}>Start</button>
+        <button  onClick={()=>{stopButton()}}>Stop</button>
+        </>
+    )
+   
+ } 
+ export default App;
---
+++ src/home.js
@@ -0,0 +1,36 @@
+
+// import React, { useState, useEffect } from 'react';
+// import "./App.css"
+
+// const App = () => {
+//   const [count, setCount] = useState(0);
+//   const [state, setState] = useState(false);
+
+//   useEffect(() => {
+//      if (state) {
+//         let interval = setInterval(() => {
+//         setCount((prevCount) => prevCount + 1);
+//       }, 500);
+//     return () => {
+//       clearInterval(interval);
+//     }};
+//   }, [state]); 
+
+//   const startButton = () => {
+//     setState(true)
+//   }
+
+//   const stopButton = () => {
+//     setState(false)
+//   };
+
+//   return (
+//     <div>
+//       <p  >Count: {count}</p>
+//       <button  onClick={startButton}>Start</button>
+//       <button   onClick={stopButton}>Stop</button>
+//     </div>
+//   )
+// }
+
+// export default App;
