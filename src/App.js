c19ac7300b43dee880fa8cfe065570bf52fa2ff4
MolakaRoopa <roopareddy8978@gmail.com>
2023-11-23 11:44:08 +0530

    message

--- src/App.js
+++ src/App.js
@@ -4,28 +4,34 @@ import"./App.css"
  const App =()=>{
     const [count,setCount]=useState(0)
     const [state,setState]=useState(false)
+    var buttonElement= document.getElementById("btn")
+    
  useEffect(()=>{
     let counter=""
     if(state){
         counter =setInterval(()=>{ 
             setCount((prevCount)=>prevCount+1)},500)
             
-    }else{
-        clearInterval(counter)
     }
      return()=>clearInterval(counter)
   },[state])
+  
   const startButton=()=>{
+   
+   buttonElement.disabled=true
+
+
     setState(true)
   }
   const stopButton=()=>{
+    buttonElement.disabled=false
     setState(false)
   }
 
     return(
         <>
         <p>Count:{count}</p>
-        <button onClick={()=>{startButton()}}>Start</button>
+        <button id="btn"  onClick={()=>{startButton()}}>Start</button>
         <button  onClick={()=>{stopButton()}}>Stop</button>
         </>
     )
